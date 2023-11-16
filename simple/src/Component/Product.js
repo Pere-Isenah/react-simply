5f929a155cb73bd47c5d2efb926721cc06174d2b
Pere-Isenah <pereisenah@gmail.com>
2023-11-16 08:03:20 +0100

    upadted

--- simple/src/Component/Product.js
+++ simple/src/Component/Product.js
@@ -1,5 +1,11 @@
+ import React from "react";
+
  const Product = ({description,name})=> {
-    return ( 
+    
+   return 
+    (Math.random() > 0.5 
+    ?(
+    <React.Fragment>
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">
@@ -8,10 +14,28 @@
           <p className="card-text">
             {description} 
           </p>
-          <a href="#" className="btn btn-primary">
+          <a href="#" className="btn btn-secondary">
             Go somewhere
           </a>
         </div>
-      </div> ); }
+      </div>
+    </React.Fragment>)
+    : (
+      <React.Fragment>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">
+              {name}
+            </h5>
+            <p className="card-text">
+              {description} 
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </React.Fragment>) )}
+
       
 export default Product; 
